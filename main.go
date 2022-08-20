34380d9be693d3935832670fc05ea448dca3c2d5
Björn Bohman <bjorn.fjallstrom@internetz.se>
2022-08-20 07:45:23 +0200

    Add cpu and mem pprof capabilitties.

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"runtime"
+	"runtime/pprof"
 	"sort"
 	"strings"
 )
@@ -16,7 +19,23 @@ type wordCounter struct {
 	totalWords uint
 }
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	reg, err := regexp.Compile("[^a-zA-Z0-9 ]+")
 	if err != nil {
@@ -68,4 +87,16 @@ func main() {
 	fmt.Printf("\nTotal number of uniq words:%10d\n", len(wordCount.words))
 	fmt.Printf("Total number of words:%15d\n", wordCount.totalWords)
 	fmt.Printf("Total number of lines:%15d\n", wordCount.totalLines)
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 }
